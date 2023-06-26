2329bdc896958df4c08f5fe2237168416393a07b
afton21 <lawverap25@gmail.com>
2023-06-25 21:28:53 -0500

    worked with some basic string methods that came with ES6.

---
+++ ES6/searchingStrings.js
@@ -0,0 +1,14 @@
+const planet = "Earth";
+// startsWith method is case sensitive
+console.log(planet.startsWith("Ear"));
+
+// endsWith method is case sensitive
+console.log(planet.endsWith("th"));
+
+// will return true or false if the entire string that
+// was passed in is in the string object that was created above.
+console.log(planet.includes("h"))
+
+// returns the index of the first occurrence of the passed in string
+console.log(planet.search("art"));
+
